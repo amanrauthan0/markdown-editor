2d1b32c359071d9bba24ab4d901fceaf629ffeb7
aman <aman44444490@gmail.com>
2026-02-11 17:33:28 +0530

    change in heading

--- markdown/src/components/header.jsx
+++ markdown/src/components/header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <header className="w-full h-14 px-6 flex items-center  border-b border-slate-700 bg-slate-900">
-  <h1 className="text-lg font-semibold text-slate-100">
+    <header className="w-full h-14 px-4 flex items-center  border-b border-slate-700 bg-slate-900">
+   <h1 className="text-2xl font-semibold text-slate-100">
     Markdown Editor
-  </h1>
+   </h1>  
 </header>
   );
 };
